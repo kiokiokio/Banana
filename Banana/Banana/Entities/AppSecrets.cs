bc5a36f61745098e27d2100995c1fc20d3e4e438
Kendra Kuhl <kendra@kuhl.ws>
2018-02-11 22:00:53 -0600

    Oauth Changes

---
+++ Banana/Banana/Entities/AppSecrets.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Banana.Entities
+{
+    public class AppSecrets
+    {
+        public string AppKey { get; set; }
+        public string SharedSecret { get; set; }
+    }
+}
